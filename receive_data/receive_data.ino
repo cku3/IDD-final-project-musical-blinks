9a25f140016af7b2f6c5f92ac67175afd4d7f127
TakoYuxin <31773666+TakoYuxin@users.noreply.github.com>
2019-11-13 16:47:32 -0500

    Simple data transferring test Arduino code
    Sending_color: code based on the example ButtonCycleColor.ino 
                            Basically when you press the button, the color of the blink changes and the blink will send color-specific data to all faces
                            RED -10, GREEN - 30, BLUE - 60
    
    Recieve_data: code based on the example ColorByNeighbor.ino 
                           It makes the blink to continuously receiving data from each face
                           when receiving 10, receiver blink turns RED
                           when receiving 30, receiver blink turns YELLOW
                           when receiving 60, receiver blinks turns GREEN

---
+++ receive_data/receive_data.ino
@@ -0,0 +1,56 @@
+/*
+ * Color By Number/Neighbor
+ * 
+ * An example showing how to count the number of neighbors.
+ * 
+ * Each Blink displays a color based on the number of neighbors around it.
+ *  
+ */
+
+// color by number of neighbors
+Color colors[] = {  
+  dim(WHITE,40),   // 0 neighbors
+  RED,            // 1 neighbors
+  YELLOW,         // 2 neighbors
+  GREEN,          // 3 neighbors
+  CYAN,           // 4 neighbors
+  BLUE,           // 5 neighbors
+  MAGENTA,        // 6 neighbors
+}; 
+
+
+
+void setup() {
+  // Blank
+}
+
+
+void loop() {
+
+  // count neighbors we have right now
+  int numNeighbors = 0;
+ 
+  FOREACH_FACE(f) {
+
+    if ( !isValueReceivedOnFaceExpired( f ) ) {   // Have we seen an neighbor on this face recently?
+          if ((int)getLastValueReceivedOnFace(f) == 10){
+            numNeighbors = 1;
+          }
+          if ((int)getLastValueReceivedOnFace(f) == 30){
+            numNeighbors = 2;
+          }
+          if ((int)getLastValueReceivedOnFace(f) == 60){
+            numNeighbors = 3;
+          }
+    
+  }
+  
+  }
+
+  // look up the color to show based on number of neighbors
+  // No need to bounds check here since we know we can never see more than 6 neighbors 
+  // because we only have 6 sides.
+  
+  setColor( colors[ numNeighbors ] );
+  
+}
